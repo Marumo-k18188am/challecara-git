ef0edf423fb2787cf3b9944f15eeadfe4b89e4cd
N4U74 <k18208nw@apps.kct.ac.jp>
2019-10-08 16:31:19 +0900

    構文の間違い等を修正

--- js/schedules_form.js
+++ js/schedules_form.js
@@ -3,10 +3,22 @@
 var db = firebase.firestore();
 var user = firebase.auth().currentUser;
 
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+
+if (user != null) {
+  name = user.displayName;
+  email = user.email;
+  photoUrl = user.photoURL;
+  emailVerified = user.emailVerified;
+  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                   // this value to authenticate with your backend server, if
+                   // you have one. Use User.getToken() instead.
+}
+
 const vm = new Vue({
     el: '#form',
     data: {
-        uid: 'user.email',
         title: '',
         time: '',
         place: '',
@@ -15,14 +27,20 @@ const vm = new Vue({
     },
 
     methods: {
-        addScedule: function(){
+        addSchedule: function(){
             db.collection("users").add({
-                title: 'this.title',
-                time: 'this.time',
-                place: 'this.palce',
-                participants: 'this.participants',
-                comment: 'this.comment',
+                title: this.title,
+                time: this.time,
+                place: this.place,
+                participants: this.participants,
+                comment: this.comment,
+                })
+            .then(function(docRef) {
+                console.log("Document written with ID: ", docRef.id);
             })
+            .catch(function(error) {
+                console.error("Error adding document: ", error);
+            });
         },
     }
 })
