2206b160dd4c53986876ba17a42ba09372b178e0
Marumo <k18188am@apps.kct.ac.jp>
2019-12-11 15:27:15 +0900

    メニューバーからプロフィール設定に飛べるように変更

--- js/userdata.js
+++ js/userdata.js
@@ -1,13 +1,20 @@
+var db = firebase.firestore();
+
 const userData=new Vue({
     el:".userData",
     data:{
         user:{},
+        mode:-1,
         hidden:true
     },
     methods:{
         initUserData(user){
             this.user=user;
             this.hidden=false;
+        },
+        getModeText(){
+            if(this.mode==0)return "個人";
+            else return "法人";
         },
         getPhotoURL(){
             return this.user.photoURL;
@@ -17,12 +24,26 @@ const userData=new Vue({
         },
         getUserId(){
             return this.user.uid;
+        },
+        setMode(mode){
+            this.mode=mode;
+        },
+        toSetProfile(){
+            window.location.href="../html/setting_profile.html";
         }
     }
 });
 
 firebase.auth().onAuthStateChanged(function(user){
     if(user){
+        db.collection("users").doc(user.uid).collection("userdata").get().then((snapshot)=>{
+            var docs=snapshot.docs;
+            docs.forEach((doc)=>{
+                if(doc.id=="mode"){
+                    userData.setMode(doc.data().mode);
+                }
+            });
+        });
         userData.initUserData(user);
     }
 });
