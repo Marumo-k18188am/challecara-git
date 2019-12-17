08cc627da712756a3df2eb2cdd785ba6f004a2c0
Marumo-k18188am <k18188am@apps.kct.ac.jp>
2019-12-18 06:48:02 +0900

    趣味チューザー追加

--- public/js/hobbyChooser.js
+++ public/js/hobbyChooser.js
@@ -1,5 +1,96 @@
 "use strict";
 
-const hobbyChooser=new Vue(){
-    
+var db = firebase.firestore();
+
+const hobbyChooser=new Vue({
+    el:"#hobbyChooser",
+    data:{
+        tags:{
+            art:[],
+            sport:[],
+            subc:[],
+            leisure:[],
+            cook:[],
+            handyc:[],
+            else:[]
+        },
+        searchedTags:[],
+        contents:{
+            art:"音楽・芸術",
+            sport:"スポーツ",
+            subc:"アニメ・ゲーム",
+            leisure:"行楽・レジャー",
+            cook:"料理",
+            handyc:"ものづくり",
+            else:"その他"
+        },
+        editingTag:{
+            genre:"",
+            name:""
+        },
+        isShown:false,
+        editingNewContent:false,
+        mode:0,
+        searchValue:"",
+        searchId:""
+    },
+    methods:{
+        showHobbyChooser(){
+            getTag();
+            this.mode=0;
+            this.isShown=true;
+        },
+        closeHobbyChooser(){
+            this.isShown=false;
+        },
+        search(searchVal,searchMode){
+            this.searchValue=searchVal;
+            if(searchMode==1){
+                var result = Object.keys(this.contents).filter((key)=>{
+                    return this.contents[key]==searchVal;
+                });
+                this.searchedTags=this.tags[result];
+                this.searchedTags.sort();
+            }
+            this.mode=1;
+        },
+        inputChanged(){
+            if(this.searchValue==""){
+                this.mode=0;
+            }else{
+                this.mode=1;
+            }
+        },
+        editNewContent(){
+            this.editingTag.genre="art";
+            this.editingTag.name="";
+            this.editingNewContent=true;
+        },
+        addContent(){
+            this.tags[this.editingTag.genre].push(this.editingTag.name);
+            db.collection("public").doc("tags").set({
+                tags:this.tags
+            }).then(()=>{
+                getTag();
+                this.editingNewContent=false;
+            });
+        },
+        closeEditContent(){
+            this.editingNewContent=false;
+        },
+        setTags(tags){
+            this.tags=tags;
+        }
+    }
+});
+
+function getTag(){
+    db.collection("public").get().then((snapshot)=>{
+        var docs=snapshot.docs;
+        docs.forEach((doc)=>{
+            if(doc.id=="tags"){
+                hobbyChooser.setTags(doc.data().tags); 
+            }
+        });
+    });
 }
