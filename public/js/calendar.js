6c9d0f3503f78ff9d1d19c608337f247ae761303
N4U74 <k18208nw@apps.kct.ac.jp>
2019-12-20 16:54:19 +0900

    フォームが法人用に飛ばない問題を修正

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -213,7 +213,12 @@ const timeschedule=new Vue({
                 }
             },
             toSchedulesForm(){
-                window.location.href="../html/schedules_form.html?mode=add&date="+this.date;
+                if(userData.mode == 0) {
+                    window.location.href="../html/schedules_form.html?mode=add&date="+this.date;   
+                }
+                else if(userData.mode == 1) {
+                    window.location.href="../html/schedules_form_corp.html?mode=add&date="+this.date;
+                }
             }
         }
     });
